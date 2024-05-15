f7896e4a8016d79d64c929a90128e05e7a2a557d
DaniBonica001 <danielabc_0106@hotmail.com>
2024-05-15 16:29:20 -0500

    Init infraestructure

---
+++ Terraform/providers.tf
@@ -0,0 +1,12 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.30"
+    }
+  }
+}
+
+provider "aws" {
+  region = var.resource_group_location
+}
---
+++ Terraform/variables.tf
@@ -0,0 +1,26 @@
+variable "resource_group_name_prefix" {
+  type = string
+  default = "rg_"
+}
+
+variable "resource_group_location" {
+  type = string
+  default = "eu-west-1"
+  description = "Location of the resource group"
+}
+
+variable "ec2_user_data" {
+  type = string
+  description = "User data to be executed on the EC2 instance"
+  default = <<-EOT
+    #!/bin/bash
+    echo "Hello Terraform!"
+    EOT
+
+}
+
+variable "vpc_cdir" {
+  type = string
+  description = "CIDR block for the VPC"
+  default = "10.0.0.0/16"
+}
