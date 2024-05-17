079e9cd23d94773a264169bacd9b887e0f5a624d
DaniBonica001 <danielabc_0106@hotmail.com>
2024-05-17 15:59:12 -0500

    Create vpc with private subnets

--- Terraform/variables.tf
+++ Terraform/variables.tf
@@ -24,3 +24,14 @@ variable "vpc_cdir" {
   description = "CIDR block for the VPC"
   default = "10.0.0.0/16"
 }
+
+variable "private_subnet_cdir_blocks"{
+  description = "CIDR blocks for the private subnets"
+  type = list(string)
+  default = [
+  "10.0.101.0/24",
+  "10.0.102.0/24",
+  "10.0.103.0/24",
+  "10.0.104.0/24"]
+
+}
