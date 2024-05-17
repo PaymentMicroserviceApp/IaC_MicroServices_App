d0ed39f1258f448591902e2028517c78b2c89196
DaniBonica001 <danielabc_0106@hotmail.com>
2024-05-17 14:23:08 -0500

    Deleted charts

---
+++ Terraform/modules/Postgres/main.tf
@@ -0,0 +1,53 @@
+provider "aws" {
+  region = "us-west-2"
+  access_key = var.access_key
+  secret_key = var.secret_key
+}
+#https://dev.to/bensooraj/accessing-amazon-rds-from-aws-eks-2pc3
+#Create the VPC
+#Create the Subnet
+#Create DB Subnet Group
+
+#Create a VPC Security Group
+resource "aws_security_group" "sg-postgres" {
+  name        = "sg-postgres"
+  description = "Allow Postgres inbound traffic"
+  #vpc_id      = var.vpc_id
+
+  ingress {
+    from_port   = 5432
+    to_port     = 5432
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_db_instance" "postgres" {
+  engine = "postgres"
+  engine_version = "14"
+  instance_class = "db.t3.micro"
+
+  storage_type = "gp2"
+  allocated_storage = 20
+  identifier = "postgres-db"
+  db_name = "db_invoice"
+
+  skip_final_snapshot          = true
+  multi_az                     = false
+  backup_retention_period      = 0
+  delete_automated_backups     = true
+  publicly_accessible          = true
+  performance_insights_enabled = false
+
+  vpc_security_group_ids = [aws_security_group.sg-postgres.id]
+  #db_subnet_group_name = var.db_subnet_group_name
+
+  tags = {
+    project = "IaC_Microservices_App"
+    created_by = "PaymentMicroserviceApp"
+  }
+
+  username = var.database_username
+  password = var.database_password
+
+}
